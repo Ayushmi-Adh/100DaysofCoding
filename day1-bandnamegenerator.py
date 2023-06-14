4ae41200706d2df447f0d9286fe2e3e5125f4a85
aayushmi adhikari <132826306+Ayushmi-Adh@users.noreply.github.com>
2023-06-14 21:18:09 +0545

    Update day1-bandnamegenerator.py

--- day1-bandnamegenerator.py
+++ day1-bandnamegenerator.py
@@ -1,7 +1,9 @@
 #This is a program to generate band names
 print("What is the name of your city?")
 name=input()
+
 print("What is your pet's name?")
 name2=input()
+
 band_name= name +" " + name2
 print (band_name)
