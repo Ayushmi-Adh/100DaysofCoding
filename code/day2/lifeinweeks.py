9bd35daa78f605763d3cba6db4a071c6cc0343b8
aayushmi adhikari <132826306+Ayushmi-Adh@users.noreply.github.com>
2023-08-19 23:12:41 -0700

    Create lifeinweeks.py

---
+++ code/day2/lifeinweeks.py
@@ -0,0 +1,10 @@
+#YOUR LIFE IN WEEKS
+
+age = int(input("Enter your age:"))
+
+years_left = 90-age
+days_left = years_left*365
+months_left = years_left*12
+weeks_left = years_left*52
+
+print(f"You have {months_left} months,{weeks_left} weeks and {days_left} days left.")
